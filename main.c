367f8960d7f956154400cb321fd7892b5e144f6f
Charles Munene <charlesmunene01@gmail.com>
2025-10-20 10:56:04 +0300

    Added a simple addition calculator

--- main.c
+++ main.c
@@ -2,5 +2,21 @@
 
 int main(void) {
     printf("Hello, World!\n");
+    
+    int num1, num2, sum;
+
+    // Ask for input
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    // Perform addition
+    sum = num1 + num2;
+
+    // Print the result
+    printf("The sum of %d and %d is %d\n", num1, num2, sum);
+
     return 0;
 }
